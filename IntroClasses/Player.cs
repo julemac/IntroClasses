61b120ae24ef439043061c2bc9669604f228871b
jule <NA@NA.NA>
2026-03-16 11:45:34 +0100

    move turn mechanics to player

--- IntroClasses/Player.cs
+++ IntroClasses/Player.cs
@@ -6,6 +6,16 @@ public class Player
     private int _y;
     private string _avatar = "@";
 
+    public int GetPositionX()
+    {
+        return _x;
+    }
+    
+    public int GetPositionY()
+    {
+        return _y;
+    }
+    
     public void Display()
     {
         Console.SetCursorPosition(_x, _y);
@@ -14,7 +24,37 @@ public class Player
 
     public void Move(int diffX, int diffY)
     {
-        _x += diffX;
-        _y += diffY;
+        if (_x + diffX < Console.WindowWidth && _x + diffX >= 0) _x += diffX;
+        if (_y + diffY < Console.WindowHeight && _y + diffY >= 0) _y += diffY;
+    }
+
+
+    public bool TakeTurn()
+    {
+        bool isPlaying = true;
+        ConsoleKeyInfo input = Console.ReadKey(true);
+        Console.SetCursorPosition(_x, _y);
+        Console.Write(".");
+        switch (input.Key)
+        {
+            case ConsoleKey.S:
+                Move(0, 1);
+                break;
+            case ConsoleKey.W:
+                Move(0, -1);
+                break;
+            case ConsoleKey.A:
+                Move(-1, 0);
+                break;
+            case ConsoleKey.D:
+                Move(1, 0);
+                break;
+            case ConsoleKey.Q: 
+                // Display();
+                isPlaying = false;
+                break;
+        } 
+        Display();
+        return isPlaying;
     }
 }
--- IntroClasses/Program.cs
+++ IntroClasses/Program.cs
@@ -7,31 +7,8 @@ public class Program
         bool isPlaying = true;
         Player hero = new Player();
         hero.Display();
-        while (isPlaying)
-        {
-            ConsoleKeyInfo input = Console.ReadKey(true);
-            switch (input.Key)
-            {
-                case ConsoleKey.S:
-                    hero.Move(0, 1);
-                    break;
-                case ConsoleKey.W:
-                    hero.Move(0, -1);
-                    break;
-                case ConsoleKey.A:
-                    hero.Move(-1, 0);
-                    break;
-                case ConsoleKey.D:
-                    hero.Move(1, 0);
-                    break;
-                case ConsoleKey.Q:
-                    isPlaying = false;
-                    break;
-            }
+        while (isPlaying = hero.TakeTurn());
 
-            hero.Display();
-        }
-        
         Console.WriteLine("Goodbye!");
     }
 }
